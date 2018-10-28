80bc2c108bf0e810264b94fb9d30ad4a73ed10cf
BChangs99 <yy3chang@edu.uwaterloo.ca>
2018-10-28 16:24:35 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -5,21 +5,18 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <div className='App-header'>
+          <div className='App-name'>Pomodoro</div>
+          <div className='App-controls'> 
+            <div className='App-control-left'>
+              Left
+            </div>
+            <div className='App-control-right'>
+              Right
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
